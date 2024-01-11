d30e06b4e88768220316f5dab015c841e92217cb
Hamed Bahram <hamed.bahram@gmail.com>
2024-01-11 12:29:35 -0500

    Initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,14 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        md: '1.5rem',
+        lg: '2rem'
+      }
+    }
   },
   plugins: [],
 }
